c021c2e5ae1681c5028a94792ab25634eba20471
kristen <kristen.chong.brown@gmail.com>
2019-02-17 13:48:43 -0700

    Update process_example.sh
    swapped to searching 2nd column for coding transcripts, this actually results in gene tracks for the example data.

--- misc/process_example.sh
+++ misc/process_example.sh
@@ -1,7 +1,7 @@
 # Modify the input example data to be smaller and easier to work with
 
 # just keep genes from original annotation file downloaded from wormbase
-awk '$3 == "gene"' c_elegans.WS230.annotations.gff3 > cel_ws230_example.gff3
+awk '$2 == "Coding_transcript"' original/c_elegans.WS230.annotations.gff3 > cel_ws230_example.gff3
 
 # sort an old wild type mRNA seq sample, remove unaligned, and subsample to 30% reads
 samtools sort n2_hits.bam | samtools view -b -F 4 | samtools view -bs 0.3 > cel_example.bam
